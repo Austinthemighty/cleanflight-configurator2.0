177faa6d660957a2257c3311d5b774ef9e7458fc
Austin Chopra <archopra100@aol.com>
2016-09-23 18:06:56 -0700

    re added the iOS files

---
+++ IOS/CleanFlight/CleanFlight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CleanFlight
+//
+//  Created by Austin Chopra on 9/23/16.
+//  Copyright © 2016 Clean-Flight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS/CleanFlight/CleanFlight/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CleanFlight
+//
+//  Created by Austin Chopra on 9/23/16.
+//  Copyright © 2016 Clean-Flight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
