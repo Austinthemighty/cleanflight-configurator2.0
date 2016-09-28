5e2d163e9a4c762d847ce249223d556ea92a0c94
Austin Chopra <archopra100@aol.com>
2016-09-27 17:50:53 -0700

    changed project over to swift

---
+++ IOS/Cleanflight Configurator/Cleanflight ConfiguratorTests/Cleanflight_ConfiguratorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cleanflight_ConfiguratorTests.swift
+//  Cleanflight ConfiguratorTests
+//
+//  Created by Austin Chopra on 9/27/16.
+//  Copyright © 2016 Clean-Flight. All rights reserved.
+//
+
+import XCTest
+@testable import Cleanflight_Configurator
+
+class Cleanflight_ConfiguratorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
